c6429786ffeab7e2fa398ccb275a6069c481e210
Jesse Hixson <hixsonj@gmail.com>
2017-01-31 08:54:41 -0500

    Adds cache and members migrations and seeds. Adds basic test for members API

---
+++ database/seeds/MemberSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Seeder;
+// use Faker;
+
+class MemberSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = Faker\Factory::create();
+        $faker->addProvider(new Faker\Provider\en_US\Person($faker));
+        $faker->addProvider(new Faker\Provider\Internet($faker));
+        $faker->addProvider(new Faker\Provider\en_US\PhoneNumber($faker));
+
+        for($i = 0; $i < 10; $i++) {
+            DB::table('members')->insert([
+                'name' => $faker->name,
+                'email' => $faker->email,
+                'phone' => $faker->phoneNumber
+            ]);
+        }
+    }
+}
---
+++ tests/MembersTest.php
@@ -0,0 +1,19 @@
+<?php
+
+use Laravel\Lumen\Testing\DatabaseMigrations;
+use Laravel\Lumen\Testing\DatabaseTransactions;
+
+class MembersTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testMemberList()
+    {
+        $response = $this->get('/members');
+        $response->assertResponseOk();
+        $response->seeJsonStructure(['error', 'data' => []]);
+    }
+}
